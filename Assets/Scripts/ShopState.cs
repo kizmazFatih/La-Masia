05cd4f14af666ce7152c9148c074dbe4b4662bb0
Emre Öngi <emreongi97@gmail.com>
2025-05-13 20:10:47 +0300

    bitisEkrani

--- Assets/Scripts/ShopState.cs
+++ Assets/Scripts/ShopState.cs
@@ -2,11 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement; // ✅ EKLEDİK
 
 public class ShopState : MonoBehaviour
 {
     public static ShopState instance;
-
 
     [Header("Day Circle")]
     public float time;
@@ -15,20 +15,15 @@ public class ShopState : MonoBehaviour
     public bool isOpen = false;
     private bool dayContinue = false;
 
-
-
-
     [Header("Shop Specials")]
-
     [Range(0, 100)] public float popularity;
     [SerializeField] private int level = 0;
 
     [Header("UI's")]
     [SerializeField] private TextMeshProUGUI clockText;
 
-
-
-
+    [Header("End of Day Screen")]
+    [SerializeField] private GameObject dayEndScreen;
 
     void Awake()
     {
@@ -45,10 +40,12 @@ public class ShopState : MonoBehaviour
     void Start()
     {
         dayContinue = true;
-
-        //Bu satır vize içindir sonradan silinecek
         EventDispatcher.SummonEvent("SetCustomerCount", popularity);
+
+        if (dayEndScreen != null)
+            dayEndScreen.SetActive(false);
     }
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.T))
@@ -59,9 +56,8 @@ public class ShopState : MonoBehaviour
         if (dayContinue)
         {
             time += 1 * Time.deltaTime;
-
-
             clockText.text = clock.ToString() + ":" + time.ToString("00");
+
             if (time >= hourLength)
             {
                 clock += 1;
@@ -69,25 +65,27 @@ public class ShopState : MonoBehaviour
                 EventDispatcher.SummonEvent("SetCustomerCount", popularity);
             }
         }
-        if (clock == 20)
+
+        if (clock == 24)
         {
             dayContinue = false;
             isOpen = false;
             Time.timeScale = 0;
             DayEnd();
-
         }
     }
 
-
     void DayEnd()
     {
-        //Yeni sahneye geç
-        //kazandığımız para
-        //Rastgele eventlerle paramız azalacak
-        //İkinci güne başla
-        //Para,popoularity kaydet
+        if (dayEndScreen != null)
+        {
+            dayEndScreen.SetActive(true);
+        }
     }
 
-
+    public void StartNewDay()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
 }
