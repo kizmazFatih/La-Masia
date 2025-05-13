b4c2179ee899be9ca6af8476b68a29af0cca4214
Emre Öngi <emreongi97@gmail.com>
2025-05-13 22:19:02 +0300

    gelir-gider text eklendi

--- Assets/Scripts/ShopState.cs
+++ Assets/Scripts/ShopState.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using UnityEngine.SceneManagement; // ✅ EKLEDİK
+using UnityEngine.SceneManagement;
 
 public class ShopState : MonoBehaviour
 {
