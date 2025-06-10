ef993f6965812b935a8b345be24aee284455081f
kizmazFatih <163738158+kizmazFatih@users.noreply.github.com>
2025-06-10 16:09:21 +0300

    chocolate syrup problem fixed

--- Assets/Scripts/RaySystem.cs
+++ Assets/Scripts/RaySystem.cs
@@ -41,14 +41,14 @@ public class RaySystem : MonoBehaviour
             {
                 if (hit.transform.GetComponent<Cup>() != null && handle.GetChild(0).tag == "Chocolate")
                 {
-                    if (Input.GetMouseButtonDown(0))
+                    if (Input.GetKeyDown(KeyCode.E))
                     {
+
                         handle.GetChild(0).GetComponent<ChocolateSyrup>().FillChocolateSyrup(hit.transform);
 
                     }
                 }
             }
-
             if (handle.childCount != 0 && handle.GetChild(0).tag == "Pistol") //Elimizde pistol varsa ışını 15 metre olarak ayarlıyoruz ve diğer etkileşimleri blokluyor
             {
                 rayDistance = 15f;
